356a1381107c17c5a053ccebd7882c79ff7ce24f
karla santos <karla.mikaelly@academico.ifpb.edu.br>
2025-04-22 09:23:05 -0300

    adicionando conexões com o bd

--- src/app/usuario/page.js
+++ src/app/usuario/page.js
@@ -3,7 +3,7 @@ export default () => (
     
         <h1>Algum texto aqui</h1>
         <p>Algum  outro texto aqui</p>
-    <p>DARLLA, sobe o terminal  {10-4}</p>
+    <p>DARLLA, sobe o terminal </p>
     
     
     </>
