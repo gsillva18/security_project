0b1100f0c6701e85c5c22846611fdb176467d43c
karla <karla.mikaelly@academico.ifpb.edu.br>
2025-08-12 08:48:54 -0300

    novas mudanças feitas, corrigido o codigo da api

--- src/app/agendamento/page.js
+++ src/app/agendamento/page.js
@@ -1,9 +1,25 @@
-export default () => {
-
-
-    return <>
-        <div><input type="checkbox"></input> Barba</div>
-        <div><input type="checkbox"></input> Corte</div> 
-        <input type="datetime-local"></input>
-    </>
+import db from "@/lib/db"
+export default async () => {
+    const usuario = await db.query("select * from usuario")
+    return (<>
+    <h1> Lista de clientes </h1>
+    <div>
+        {
+            usuario.rows.map(
+                c => (
+                    <div>
+                        {c.nome} 
+                   </div>
+                )
+            )
+        }
+    </div>
+ </>);
 }
+
+
+   // return <>
+     //   <div><input type="checkbox"></input> Barba</div>
+       // <div><input type="checkbox"></input> Corte</div> 
+       // <input type="datetime-local"></input>
+    
