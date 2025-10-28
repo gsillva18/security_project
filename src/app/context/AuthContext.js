62b6e603f05351e0f42e9c94d8d0c897ddfd5d6d
gsillva18 <josegabrielg831@gmail.com>
2025-10-28 14:39:25 -0300

    update

--- src/app/context/AuthContext.js
+++ src/app/context/AuthContext.js
@@ -1,32 +1,32 @@
-'use client'
+// 'use client'
 
-import { createContext, useContext, useState, useEffect } from 'react'
+// import { createContext, useContext, useState, useEffect } from 'react'
 
-export const AuthContext = createContext()
+// export const AuthContext = createContext()
 
-export function AuthProvider({ children }) {
-  const [usuarioLogado, setUsuarioLogado] = useState(null)
+// export function AuthProvider({ children }) {
+//   const [usuarioLogado, setUsuarioLogado] = useState(null)
 
-  // Recupera usuário do localStorage ao carregar o app
-  useEffect(() => {
-    const usuario = localStorage.getItem('usuario')
-    if (usuario) setUsuarioLogado(JSON.parse(usuario))
-  }, [])
+//   // Recupera usuário do localStorage ao carregar o app
+//   useEffect(() => {
+//     const usuario = localStorage.getItem('usuario')
+//     if (usuario) setUsuarioLogado(JSON.parse(usuario))
+//   }, [])
 
-  // Atualiza localStorage quando usuarioLogado muda
-  useEffect(() => {
-    if (usuarioLogado) {
-      localStorage.setItem('usuario', JSON.stringify(usuarioLogado))
-    } else {
-      localStorage.removeItem('usuario')
-    }
-  }, [usuarioLogado])
+//   // Atualiza localStorage quando usuarioLogado muda
+//   useEffect(() => {
+//     if (usuarioLogado) {
+//       localStorage.setItem('usuario', JSON.stringify(usuarioLogado))
+//     } else {
+//       localStorage.removeItem('usuario')
+//     }
+//   }, [usuarioLogado])
 
-  return (
-    <AuthContext.Provider value={{ usuarioLogado, setUsuarioLogado }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
+//   return (
+//     <AuthContext.Provider value={{ usuarioLogado, setUsuarioLogado }}>
+//       {children}
+//     </AuthContext.Provider>
+//   )
+// }
 
-export const useAuth = () => useContext(AuthContext)
+// export const useAuth = () => useContext(AuthContext)
